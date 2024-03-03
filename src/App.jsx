6d294248ac890c0613d057f6c7b4cf5a7ce99bdd
Gaston <gastonallerdev@gmail.com>
2024-03-03 11:04:50 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Companies from "./components/Companies/Companies";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Residencies from "./components/Residencies/Residencies";
+// import Residencies from "./components/Residencies/Residencies";
 function App() {
   return (
     <div className="App">
