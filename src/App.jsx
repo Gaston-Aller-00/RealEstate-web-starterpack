140f3dcba6ddf3aa396b711a104da5d6cce5c6d3
Gaston <gastonallerdev@gmail.com>
2024-03-03 09:50:14 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Header from "../../client2/client/src/components/Header/Header";
-import Hero from "../../client2/client/src/components/Hero/Hero";
 import "./App.css"
+import Companies from "./components/Companies/Companies";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Companies/>
     </div>
   );
 }
