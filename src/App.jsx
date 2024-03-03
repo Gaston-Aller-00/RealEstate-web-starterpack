81ec0c74a989b7131321015a17d1a460bc3c82df
Gaston <gastonallerdev@gmail.com>
2024-03-03 11:03:28 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import "./App.css"
+import "./App.css";
 import Companies from "./components/Companies/Companies";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Residencies from "./components/Residencies/Residencies";
 function App() {
   return (
     <div className="App">
       <div>
-        <div className="white-gradient"/>
+        <div className="white-gradient" />
         <Header />
         <Hero />
       </div>
-      <Companies/>
+      <Companies />
+      <Residencies />
     </div>
   );
 }
