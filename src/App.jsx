b642a6df68d26653ce4b483be1b5b7068038e15a
Gaston <gastonallerdev@gmail.com>
2024-03-03 09:28:03 -0300

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
+import Header from "../../client2/client/src/components/Header/Header";
+import Hero from "../../client2/client/src/components/Hero/Hero";
 import "./App.css"
 function App() {
   return (
