022d51a0b017cb3b37482f03343ad5800112c6a6
Gaston <gastonallerdev@gmail.com>
2024-03-04 12:01:48 -0300

    last

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Value.css";
 import {
   Accordion,
@@ -11,7 +11,21 @@ import {
 import "react-accessible-accordion/dist/fancy-example.css";
 import data from "../../utils/accordion";
 import { MdOutlineArrowDropDown } from "react-icons/md";
+
 const Value = () => {
+  const [accordionState, setAccordionState] = useState([]);
+
+  useEffect(() => {
+    const newAccordionState = Array(data.length).fill("collapsed");
+    setAccordionState(newAccordionState);
+  }, []);
+
+  const handleAccordionToggle = (index) => {
+    const newAccordionState = [...accordionState];
+    newAccordionState[index] = accordionState[index] === "collapsed" ? "expanded" : "collapsed";
+    setAccordionState(newAccordionState);
+  };
+
   return (
     <section className="v-wrapper">
       <div className="paddings innerWidth flexCenter v-container">
@@ -36,38 +50,43 @@ const Value = () => {
             allowMultipleExpanded={false}
             preExpanded={[0]}
           >
-            {data.map((item, index) => {
-                const[className,setClassName] = useState(null)
-              return (
-                <AccordionItem
-                  className={`accordionItem ${className} `}
-                  key={index}
-                  uuid={index}
-                >
-                  <AccordionItemHeading>
-                    <AccordionItemButton className="flexCenter accordionButton">
-                      <AccordionItemState>
-                        {({ expanded }) =>
-                          expanded
-                            ? setClassName("expanded")
-                            : setClassName("collapsed")
-                        }
-                      </AccordionItemState>
+            {data.map((item, index) => (
+              <AccordionItem
+                className={`accordionItem ${accordionState[index]} `}
+                key={index}
+                uuid={index}
+              >
+                <AccordionItemHeading>
+                  <AccordionItemButton
+                    className="flexCenter accordionButton"
+                    onClick={() => handleAccordionToggle(index)}
+                  >
+                    <AccordionItemState>
+                      {({ expanded }) =>
+                        expanded ? (
+                          <div className="flexCenter icon expanded">
+                            {item.icon}
+                          </div>
+                        ) : (
+                          <div className="flexCenter icon collapsed">
+                            {item.icon}
+                          </div>
+                        )
+                      }
+                    </AccordionItemState>
 
-                      <div className="flexCenter icon">{item.icon}</div>
-                      <span className="primaryText">{item.heading}</span>
-                      <div className="flexCenter icon">
-                        <MdOutlineArrowDropDown size={20} />
-                      </div>
-                    </AccordionItemButton>
-                  </AccordionItemHeading>
+                    <span className="primaryText">{item.heading}</span>
+                    <div className="flexCenter icon">
+                      <MdOutlineArrowDropDown size={20} />
+                    </div>
+                  </AccordionItemButton>
+                </AccordionItemHeading>
 
-                  <AccordionItemPanel>
-                    <p className="secondaryText">{item.detail}</p>
-                  </AccordionItemPanel>
-                </AccordionItem>
-              );
-            })}
+                <AccordionItemPanel>
+                  <p className="secondaryText">{item.detail}</p>
+                </AccordionItemPanel>
+              </AccordionItem>
+            ))}
           </Accordion>
         </div>
       </div>
